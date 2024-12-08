19c08598a759d15845048e9aa0c004c4ea246f3d
Robert McGuinness <robert.mcguinness.iii@gmail.com>
2024-12-08 12:29:59 -0500

    fix: tailwind custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 
 module.exports = {
-  content: ["*.{html,js}"],
-  theme: {
-    extend: { 
-       screens: {
-        "sm":"480px"
-       },
-      fontfamily: {
-        'sixfour':["Sixtyfour Convergence", "serif"]
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ["./**/*.{html,js}"],
+	theme: {
+		extend: {
+			screens: {
+				sm: "480px",
+			},
+			fontFamily: {
+				sixtyfour: ["Sixtyfour Convergence", "serif"],
+			},
+		},
+	},
+	plugins: [],
+};
